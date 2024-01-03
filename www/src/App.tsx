72cceccc1d383c19315c7013a0b9d43bc28f1dd9
Saverio <svscagn@gmail.com>
2024-01-04 00:17:31 +0100

    create www

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,5 +1,13 @@
+import { MDXProvider } from "@mdx-js/react";
+
+import Hello from "../../docs/hello.mdx";
+
 function App() {
-  return <>title</>;
+  return (
+    <MDXProvider>
+      <Hello />
+    </MDXProvider>
+  );
 }
 
 export default App;
--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mdx from "@mdx-js/rollup";
+import remarkBreaks from "remark-breaks";
+import remarkGfm from "remark-gfm";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), mdx({ remarkPlugins: [remarkBreaks, remarkGfm] })]
+});
