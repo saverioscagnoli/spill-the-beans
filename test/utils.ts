989522172d53f19319114f8a99b8413e4327fa0c
Saverio <svscagn@gmail.com>
2024-01-03 12:28:04 +0100

    move test script and fix paths

--- test/utils.ts
+++ test/utils.ts
@@ -1,6 +1,6 @@
-import { Entry } from "../src/main/structs";
+import { Entry } from "../app/src/main/structs";
 import { faker } from "@faker-js/faker";
-import { rng } from "../src/main/lib";
+import { rng } from "../app/src/main/lib";
 
 function flip(n: number = 50): boolean {
   return rng({ min: 0, max: 100 }) <= n;
@@ -12,7 +12,7 @@ function generateRandomEnties(n: number = 1000): Entry[] {
     password: faker.internet.password(),
     email: flip(rng({ min: 0, max: 100 })) ? faker.internet.email() : null,
     icon: flip(rng({ min: 0, max: 100 })) ? faker.image.avatar() : null
-  }));
+  })) as Entry[];
 }
 
 export { flip, generateRandomEnties };
